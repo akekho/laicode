aa8b02246229a2f977c0199abaa8925d00105926
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-04 10:55:59 -0700

    Update code.java

--- String Replace/code.java
+++ String Replace/code.java
@@ -15,6 +15,8 @@
 //
 // use StringBuilder
 //
+// 每次找到source的index，从那个index开始把target加到StringBuilder末尾，
+// 直到再也找不到source为止，最后把剩下的部分加上即可
 public class Solution {
   public String replace(String input, String source, String target) {
     StringBuilder sb = new StringBuilder();
@@ -34,3 +36,104 @@ public class Solution {
     return sb.toString();
   }
 }
+
+
+
+// not using any String or StringBuilder utility, 
+// and using char[] to do it "in place"
+public class Solution {
+  public String replace(String input, String source, String target) {
+    char[] charArray = input.toCharArray();
+    
+    if (source.length() >= target.length()) {
+      return replaceShorter(charArray, source, target);
+    }
+    else {
+      return replaceLonger(charArray, source, target);
+    }
+  }
+  
+  private String replaceShorter(char[] input, String s, String t) {
+    int slow = 0;
+    int fast = 0;
+    while (fast < input.length) {
+      if (fast <= input.length - s.length() && equalSubstring(input, fast, s)) {
+        copySubstring(input, slow, t);
+        slow += t.length();
+        fast += s.length();
+      }
+      else {
+        input[slow++] = input[fast++];
+      }
+    }
+    
+    return new String(input, 0, slow);   
+  }
+  
+  private String replaceLonger(char[] input, String s, String t) {
+    ArrayList<Integer> matches = getAllMatches(input, s);
+    
+    char[] result = new char[input.length + matches.size() * (t.length() - s.length())];
+    
+    int lastIndex = matches.size() - 1;
+    int fast = input.length - 1;
+    int slow = result.length - 1;
+    
+    while (fast >= 0) {
+      if (lastIndex >= 0 && fast == matches.get(lastIndex)) {
+        copySubstring(result, slow - t.length() + 1, t);
+        slow -= t.length();
+        fast -= s.length();
+        lastIndex--;        
+      }
+      else {
+        result[slow--] = input[fast--];
+      }
+    }
+    
+    return new String(result);
+  }
+  
+  private boolean equalSubstring(char[] input, int fromIndex, String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (input[fromIndex + i] != s.charAt(i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  private void copySubstring(char[] result, int fromIndex, String t) {
+    for (int i = 0; i < t.length(); i++) {
+      result[fromIndex + i] = t.charAt(i);
+    }
+  }
+  
+  private ArrayList<Integer> getAllMatches(char[] input, String s) {
+    ArrayList<Integer> matches = new ArrayList<>();
+    
+    int i = 0; 
+    
+    while (i <= input.length - s.length()) {
+      if (equalSubstring(input, i, s)) {
+        matches.add(i + s.length() - 1);
+        i += s.length();
+      }
+      else {
+        i++;
+      }
+    }
+    
+    return matches;
+  }
+}
+
+
+
+
+// Professional solution
+//
+https://docs.google.com/document/d/1FqVLq6Pu6FMr0cObsjUskr7MNLVZfCiTKOrbrTaB0ko/edit#heading=h.70h4zzxhb0wk
+
+
+
