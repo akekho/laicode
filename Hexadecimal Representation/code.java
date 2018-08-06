c32257c5df5c5404b1214ab8ae642358b352af6d
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-05 23:03:08 -0700

    Update code.java

--- Hexadecimal Representation/code.java
+++ Hexadecimal Representation/code.java
@@ -13,6 +13,8 @@
 
 // My solution
 //
+// Solution 1 (works only when the input is non-negative):
+//
 public class Solution {
   public String hex(int number) {
     if (number == 0) {
@@ -34,6 +36,32 @@ public class Solution {
   }
 }
 
+
+
+
+// Solution 2 (general solution):
+//
+public class Solution {
+  public String hex(int number) {
+    if (number == 0) {
+      return "0x0";
+    }
+    
+    char[] charArray = {'0', '1', '2', '3', '4', '5', '6', '7', 
+                        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    StringBuilder sb = new StringBuilder("0x");
+    boolean isLeadingZero = true;
+    
+    for (int last_4_bit = 28; last_4_bit >= 0; last_4_bit -= 4) {
+      char hex = charArray[(number >> last_4_bit) & 0xF];
+      if (hex != '0' || !isLeadingZero) {
+        sb.append(hex);
+        isLeadingZero = false;
+      }
+    }    
+    return sb.toString();
+  }
+}
 
 
 
