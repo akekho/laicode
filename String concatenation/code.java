7e7f1e1c9c9765e3404fbfe716f0f0c86aac2e42
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-08 17:12:38 -0700

    Create code.java

---
+++ String concatenation/code.java
@@ -0,0 +1,18 @@
+Given two strings, concatenate them into one, separated by a comma.
+
+Example:
+
+first string: “hello”
+
+second string: “world”
+
+result: “hello,world”
+
+public class Solution {
+  public String concatenate(String x, String y) {
+    return x + "," + y;
+  }
+}
+
+
+
