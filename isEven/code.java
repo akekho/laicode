7f81c39e5967ca6c1ca45c3cd0c4ecfb0a0f55ea
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-08 17:11:02 -0700

    Create code.java

---
+++ isEven/code.java
@@ -0,0 +1,11 @@
+Check if the number x is even, return true for even numbers.
+
+public class Solution {
+  public boolean isEven(int x) {
+    boolean b = x % 2 == 0 ? true : false;
+    return b;
+  }
+}
+
+
+
