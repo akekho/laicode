1cb33ea1e4b5e684a655738d4ec7426d0ddbd212
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-18 09:27:38 -0700

    Update code.java

--- Largest SubArray Sum/code.java
+++ Largest SubArray Sum/code.java
@@ -39,3 +39,45 @@ https://docs.google.com/document/d/1FqVLq6Pu6FMr0cObsjUskr7MNLVZfCiTKOrbrTaB0ko/
 
 
 
+
+Follow up: how to return the left-right border of the solution?
+
+//Use temporary variables to store: global_start, global_end, curr_start, curr_end
+//其实curr_end是不必要的，因为curr_end永远等于i，因此只需要3个变量即可
+  
+    public int largestSum(int[] array) {
+        if (array.length == 1) {
+            return array[0];
+        }
+
+        int last = array[0];
+        int global_max = array[0];
+        int g_start = 0;
+        int g_end = 0;
+        int curr_start = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            // 如果last < 0，需要抛弃之前记录的结果，start重新开始记录
+            // 否则更新last
+            if (last < 0) {
+                curr_start = i;
+                last = array[i];
+            }
+            else {
+                last += array[i];
+            }
+
+            // 如果global_max需要更新，那么同时更新g_start和g_end
+            if (last > global_max) {
+                g_start = curr_start;
+                g_end = i;
+                global_max = last;
+            }
+        }
+
+        System.out.println("start: " + g_start + ", end: " + g_end);
+        return global_max;
+    }
+
+
+
