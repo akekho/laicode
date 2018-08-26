28e779e894f99878e61c587fdd62be28fadac948
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-08-26 00:22:59 -0700

    Create code.java

---
+++ Maximum Path Sum Binary Tree II/code.java
@@ -0,0 +1,76 @@
+//Given a binary tree in which each node contains an integer number. 
+//Find the maximum possible sum from any node to any node (the start node and the end node can be the same). 
+
+//Assumptions
+//
+//The root of the given binary tree is not null
+
+//Examples
+//     -1
+//    /    \
+//  2      11
+//       /    \
+//      6    -14
+//
+//one example of paths could be -14 -> 11 -> -1 -> 2
+//another example could be the node 11 itself
+//The maximum path sum in the above binary tree is 6 + 11 + (-1) + 2 = 18
+
+//How is the binary tree represented?
+//We use the level order traversal sequence with a special symbol "#" denoting the null node.
+//For Example:
+//The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+//      1
+//    /   \
+//   2     3
+//        /
+//      4
+
+
+
+
+
+// My solution
+//
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+    private int max = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        getPath(root);
+        return max;
+    }
+    
+    private int getPath(TreeNode root) {            
+        if (root == null) {
+            return 0;
+        }
+        // step 1：以当前节点为LCA的左右子树沿着一条path延伸的最大sum 
+        // (即每次要么向左要么向右)
+        int left = Math.max(getPath(root.left), 0);
+        int right = Math.max(getPath(root.right), 0);
+        // step 2：更新max
+        max = Math.max(max, left + right + root.key);
+        // step 3：same as step 1
+        return Math.max(left, right) + root.key;
+    }  
+}
+
+
+
+
+// Professional solution
+//
+https://docs.google.com/document/d/1FqVLq6Pu6FMr0cObsjUskr7MNLVZfCiTKOrbrTaB0ko/edit#heading=h.lnl6xfrfdrrv
+
+
+
