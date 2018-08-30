20e5907181a6aa7b3accd8ed8ebada05420ccb4a
Inskey <yanzhili@usc.edu>
2018-08-30 10:13:39 -0700

    Create code.java

---
+++ Array Deduplication I/code.java
@@ -0,0 +1,49 @@
+//Given a sorted integer array, remove duplicate elements. 
+//For each group of elements with the same value keep only one of them. 
+//Do this in-place, using the left side of the original array 
+//and maintain the relative order of the elements of the array. 
+//Return the array after deduplication.
+
+//Assumptions
+//
+//The array is not null
+
+//Examples
+//
+//{1, 2, 2, 3, 3, 3} → {1, 2, 3}
+
+
+
+// My solution
+//
+public class Solution {
+  public int[] dedup(int[] array) {
+    if (array.length < 2) {
+      return array;
+    }
+    
+    int slow = 0; 
+    
+    for (int fast = 0; fast < array.length; fast++) {
+      if (array[fast] != array[slow]) {
+        array[++slow] = array[fast];
+      }
+    }
+    
+    int[] result = new int[slow + 1];
+    for (int i = 0; i < result.length; i++) {
+      result[i] = array[i];
+    }
+    return result;
+  }
+}
+
+
+
+
+// Professional solution
+//
+https://docs.google.com/document/d/1FqVLq6Pu6FMr0cObsjUskr7MNLVZfCiTKOrbrTaB0ko/edit#heading=h.pyediyikg988
+
+
+
