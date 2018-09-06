01f2a8a26fd7c5a93005396990a62009a161ca46
Inskey <yanzhili@usc.edu>
2018-09-05 22:19:05 -0700

    Create code.java

---
+++ Node Comparator/code.java
@@ -0,0 +1,27 @@
+//Write a comparator for the class Node so that the comparator can be used in a MinHeap.
+
+
+
+// My solution
+//
+class Node {
+	int value;
+	public Node(int v) {
+		value = v;
+	}
+}
+
+class NodeComparator implements Comparator<Node> {
+
+	@Override
+	public int compare(Node o1, Node o2) {
+    if (o1.value == o2.value) {
+      return 0;
+    }
+    return o1.value < o2.value ? -1 : 1;
+	}
+	
+}
+
+
+
