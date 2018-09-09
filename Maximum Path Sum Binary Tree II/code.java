e9557f76e098d6f6da9a949364ac7cd3a9d19e49
Inskey <yanzhili@usc.edu>
2018-09-09 11:41:56 -0700

    Update code.java

--- Maximum Path Sum Binary Tree II/code.java
+++ Maximum Path Sum Binary Tree II/code.java
@@ -43,23 +43,22 @@
  * }
  */
 public class Solution {
-    private int max = Integer.MIN_VALUE;
-    
     public int maxPathSum(TreeNode root) {
-        getPath(root);
-        return max;
+        int[] max = {Integer.MIN_VALUE};
+        getPath(root, max);
+        return max[0];
     }
     
-    private int getPath(TreeNode root) {            
+    private int getPath(TreeNode root, int[] max) {            
         if (root == null) {
             return 0;
         }
         // step 1：以当前节点为LCA的左右子树沿着一条path延伸的最大sum 
         // (即每次要么向左要么向右)
-        int left = Math.max(getPath(root.left), 0);
-        int right = Math.max(getPath(root.right), 0);
+        int left = Math.max(getPath(root.left, max), 0);
+        int right = Math.max(getPath(root.right, max), 0);
         // step 2：更新max
-        max = Math.max(max, left + right + root.key);
+        max[0] = Math.max(max[0], left + right + root.key);
         // step 3：same as step 1
         return Math.max(left, right) + root.key;
     }  
